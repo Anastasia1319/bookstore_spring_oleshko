c141ce6f583ff809935f7a7309e258108180f4c5
Anastasiya Oleshko <anastasiya.oleshko13@gmail.com>
2022-12-04 22:49:37 +0300

    Added methods to the UserDao interface

--- src/com/belhard/bookstore/data/dao/UserDao.java
+++ src/com/belhard/bookstore/data/dao/UserDao.java
@@ -1,4 +1,14 @@
 package com.belhard.bookstore.data.dao;
 
+import com.belhard.bookstore.data.entity.User;
+
+import java.util.List;
+
 public interface UserDao {
+    List<User> findAll();
+    User create (User user);
+    User update (User user);
+    boolean delete (Long id);
+    User findByEmail(String email);
+    int countAll ();
 }
