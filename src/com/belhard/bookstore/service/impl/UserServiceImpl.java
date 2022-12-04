378642e3f2f096def6aabfbe9026c5976964d19a
Anastasiya Oleshko <anastasiya.oleshko13@gmail.com>
2022-12-05 00:35:13 +0300

    Created UserServiceImpl. Implemented methods from UserService

---
+++ src/com/belhard/bookstore/service/impl/UserServiceImpl.java
@@ -0,0 +1,33 @@
+package com.belhard.bookstore.service.impl;
+
+import com.belhard.bookstore.service.UserService;
+import com.belhard.bookstore.service.dto.UserDto;
+
+import java.util.List;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public List<UserDto> getAll() {
+        return null;
+    }
+
+    @Override
+    public UserDto getByEmail(String email) {
+        return null;
+    }
+
+    @Override
+    public UserDto create(UserDto dto) {
+        return null;
+    }
+
+    @Override
+    public UserDto update(UserDto dto) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+}
