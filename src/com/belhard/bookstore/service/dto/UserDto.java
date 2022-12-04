5f090139c26628b41ef578efb69b3d4bb6ea34c4
Anastasiya Oleshko <anastasiya.oleshko13@gmail.com>
2022-12-05 00:23:41 +0300

    Created UserDto class. Added fields

---
+++ src/com/belhard/bookstore/service/dto/UserDto.java
@@ -0,0 +1,13 @@
+package com.belhard.bookstore.service.dto;
+
+import com.belhard.bookstore.data.entity.Role;
+
+public class UserDto {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private Role role;
+    
+}
