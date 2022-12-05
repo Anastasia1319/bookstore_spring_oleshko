a4b25657448c154f2cc7014919d93ae3abc7ced5
Anastasiya Oleshko <anastasiya.oleshko13@gmail.com>
2022-12-05 16:39:42 +0300

    Created log for connection

--- src/main/java/com/belhard/bookstore/data/connection/DataSource.java
+++ src/main/java/com/belhard/bookstore/data/connection/DataSource.java
@@ -1,10 +1,14 @@
 package com.belhard.bookstore.data.connection;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DataSource {
+    private static final Logger logger = LogManager.getLogger(DataSource.class);
     private static final String URL = "jdbc:postgresql://localhost:5432/bookstore_bh";
     private static final String USER = "postgres";
     private static final String PASSWORD = "root";
@@ -12,7 +16,9 @@ public class DataSource {
 
     public Connection getConnection() {
         try {
-            return connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            logger.info("Is connected");
+            return connection;
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
