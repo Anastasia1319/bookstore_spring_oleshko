8d83cdc508cb53bbc0691d3f01371e00200b2ce5
Anastasiya Oleshko <anastasiya.oleshko13@gmail.com>
2022-12-20 11:36:58 +0300

    Created config.properties. Corrected DataSource

--- src/main/java/com/belhard/bookstore/data/connection/DataSource.java
+++ src/main/java/com/belhard/bookstore/data/connection/DataSource.java
@@ -3,28 +3,42 @@ package com.belhard.bookstore.data.connection;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DataSource {
     private static final Logger log = LogManager.getLogger(DataSource.class);
-    private static final String URL = "jdbc:postgresql://surus.db.elephantsql.com/llpjwimb";
-    private static final String USER = "llpjwimb";
-    private static final String PASSWORD = "J7F660-xGX74oV9lT1bGlB-IAs2Z33gf";
+    private static final String PATH_TO_PROPERTIES = "/config.properties";
+
     private Connection connection;
 
     public Connection getConnection() {
+        initConnection();
+        return connection;
+    }
+
+    private void initConnection() {
         try {
             Class.forName("org.postgresql.Driver");
-            Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
-            log.info("Is connected");
-            return connection;
+            try (InputStream in = getClass().getResourceAsStream(PATH_TO_PROPERTIES)) {
+                Properties properties = new Properties();
+                properties.load(in);
+                connection = DriverManager.getConnection(properties.getProperty("URL"), properties.getProperty("USER"),
+                        properties.getProperty("PASSWORD"));
+                log.info("Is connected");
+            }
         } catch (SQLException e) {
             log.error("Database connection not created: ", e);
             throw new RuntimeException(e);
         } catch (ClassNotFoundException e) {
             log.error("Not found Driver class for connection with PostgreSQL");
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            log.error("Failed to read config.properties", e);
             throw new RuntimeException(e);
         }
     }
