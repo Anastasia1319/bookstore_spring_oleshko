b9a9d595e62755e439ed02c275ade0ed3f87b433
Anastasiya Oleshko <anastasiya.oleshko13@gmail.com>
2022-12-26 16:55:56 +0300

    Annotation changed

--- src/main/java/com/belhard/bookstore/data/connection/DataSource.java
+++ src/main/java/com/belhard/bookstore/data/connection/DataSource.java
@@ -3,17 +3,16 @@ package com.belhard.bookstore.data.connection;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-@Component
+@Repository
 public class DataSource {
     private static final Logger log = LogManager.getLogger(DataSource.class);
-    private static final String PATH_TO_PROPERTIES = "/application.properties";
     @Value("${db.remote.url}")
     private static String url;
     @Value("${db.remote.user}")
