34d644afe57b333d113677ecdd48ee5b88d5e738
Anastasiya Oleshko <anastasiya.oleshko13@gmail.com>
2022-12-26 15:34:56 +0300

    Added javax.annotation dependency to pom.xml. Added init and destroy methods to FrontController class

--- src/main/java/com/belhard/bookstore/controller/FrontController.java
+++ src/main/java/com/belhard/bookstore/controller/FrontController.java
@@ -1,5 +1,6 @@
 package com.belhard.bookstore.controller;
 
+import com.belhard.bookstore.AppConfig;
 import com.belhard.bookstore.controller.command.Command;
 import com.belhard.bookstore.controller.command.impl.CommandFactory;
 import jakarta.servlet.ServletException;
@@ -7,11 +8,21 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import java.io.IOException;
 
 @WebServlet("/controller")
 public class FrontController extends HttpServlet {
+    private AnnotationConfigApplicationContext context;
+
+    @Override
+    public void init() throws ServletException {
+        context = new AnnotationConfigApplicationContext(AppConfig.class);
+    }
+    @Override
+    public void destroy() {
+        context.close();
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
