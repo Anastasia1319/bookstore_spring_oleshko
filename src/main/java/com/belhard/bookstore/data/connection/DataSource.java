1ec43957a33674f8f46d3ba4da9a9f01dff6e82a
Anastasiya Oleshko <anastasiya.oleshko13@gmail.com>
2022-12-26 15:13:46 +0300

    Added a pointer to the application.properties file in the AppConfig class. Changes have been made to the DataSource class for connecting to the database

--- src/main/java/com/belhard/bookstore/data/connection/DataSource.java
+++ src/main/java/com/belhard/bookstore/data/connection/DataSource.java
@@ -2,19 +2,23 @@ package com.belhard.bookstore.data.connection;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
-import java.io.IOException;
-import java.io.InputStream;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
+@Component
 public class DataSource {
     private static final Logger log = LogManager.getLogger(DataSource.class);
     private static final String PATH_TO_PROPERTIES = "/application.properties";
+    @Value("${db.remote.url}")
     private static String url;
+    @Value("${db.remote.user}")
     private static String user;
+    @Value("${db.remote.password}")
     private static String password;
 
     private Connection connection;
@@ -27,35 +31,14 @@ public class DataSource {
     private void initConnection() {
         try {
             Class.forName("org.postgresql.Driver");
-            try (InputStream in = getClass().getResourceAsStream(PATH_TO_PROPERTIES)) {
-                Properties properties = new Properties();
-                properties.load(in);
-                getConnectionConfig();
-                connection = DriverManager.getConnection(properties.getProperty(url), properties.getProperty(user),
-                        properties.getProperty(password));
-                log.info("Is connected");
-            }
+            connection = DriverManager.getConnection(url, user, password);
+            log.info("Is connected");
         } catch (SQLException e) {
             log.error("Database connection not created: ", e);
             throw new RuntimeException(e);
         } catch (ClassNotFoundException e) {
             log.error("Not found Driver class for connection with PostgreSQL");
             throw new RuntimeException(e);
-        } catch (IOException e) {
-            log.error("Failed to read application.properties", e);
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static void getConnectionConfig() {
-        if (ConfigManager.INSTANCE.getConfig("db").equals("remote")) {
-            url = "db.remote.url";
-            user = "db.remote.user";
-            password = "db.remote.password";
-        } else {
-            url = "db.local.url";
-            user = "db.local.user";
-            password = "db.local.password";
         }
     }
 }
