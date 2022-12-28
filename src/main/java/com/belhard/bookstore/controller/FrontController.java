e5e06b3cb773ef4932bb5444bfef842604bd5395
Anastasiya Oleshko <anastasiya.oleshko13@gmail.com>
2022-12-28 16:13:41 +0300

    Added logs to FrontController class

--- src/main/java/com/belhard/bookstore/controller/FrontController.java
+++ src/main/java/com/belhard/bookstore/controller/FrontController.java
@@ -7,20 +7,25 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import java.io.IOException;
 
 @WebServlet("/controller")
 public class FrontController extends HttpServlet {
     private AnnotationConfigApplicationContext context;
+    private static final Logger log = LogManager.getLogger(FrontController.class);
 
     @Override
     public void init() {
         context = new AnnotationConfigApplicationContext(AppConfig.class);
+        log.info("Created context");
     }
     @Override
     public void destroy() {
         context.close();
+        log.info("Context is closed");
     }
 
     @Override
@@ -29,8 +34,10 @@ public class FrontController extends HttpServlet {
         Command command = context.getBean(action, Command.class);
         String page;
         try {
+            log.info("DoGet method is started");
             page = command.execute(req);
         } catch (Exception e) {
+            log.error("Context is not created", e);
             req.setAttribute("Error_message", "Sorry!... Incorrect request");
             resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
             page = context.getBean("error", Command.class).execute(req);
