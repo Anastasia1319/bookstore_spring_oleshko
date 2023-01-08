4f90e1ff0691bfd9356cd74b0c6ca1bb51be9549
Anastasiya Oleshko <anastasiya.oleshko13@gmail.com>
2023-01-08 22:54:18 +0300

    Added OrderItem list to Order field

--- src/main/java/com/belhard/bookstore/data/entity/Order.java
+++ src/main/java/com/belhard/bookstore/data/entity/Order.java
@@ -3,12 +3,14 @@ package com.belhard.bookstore.data.entity;
 import lombok.Data;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Data
 public class Order {
     private Long id;
     private User user;
     private BigDecimal totalCost;
+    private List<OrderItem> items;
     private Status status;
 
     public enum Status {
