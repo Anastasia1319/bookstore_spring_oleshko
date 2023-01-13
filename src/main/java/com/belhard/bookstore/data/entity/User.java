bbed035c5e51a466dc466429ca0db0a9c752ebcc
Anastasiya Oleshko <anastasiya.oleshko13@gmail.com>
2023-01-13 16:46:34 +0300

    Added annotation in user entity for hibernate

--- src/main/java/com/belhard/bookstore/data/entity/User.java
+++ src/main/java/com/belhard/bookstore/data/entity/User.java
@@ -2,12 +2,33 @@ package com.belhard.bookstore.data.entity;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
 @Data
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long id;
+
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
+
+    @Column(name = "email")
     private String email;
+
+    @Column(name = "password")
     private String password;
+
+    @Column(name = "role_id")
     private Role role;
+
+    @Column(name = "is_active")
+    private boolean isActive;
 }
