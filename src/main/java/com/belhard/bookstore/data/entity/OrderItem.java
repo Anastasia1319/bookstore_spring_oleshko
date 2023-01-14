5ba1c81e83895cba12dc06051dccc8305c7278cb
Anastasiya Oleshko <anastasiya.oleshko13@gmail.com>
2023-01-14 13:45:02 +0300

    Added annotations for hibernate in OrderItem entity

--- src/main/java/com/belhard/bookstore/data/entity/OrderItem.java
+++ src/main/java/com/belhard/bookstore/data/entity/OrderItem.java
@@ -2,13 +2,34 @@ package com.belhard.bookstore.data.entity;
 
 import lombok.Data;
 
+import javax.persistence.*;
 import java.math.BigDecimal;
 
+@Entity
+@Table(name = "order_items")
 @Data
 public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @ManyToOne(cascade = {CascadeType.PERSIST,
+                    CascadeType.MERGE,
+                    CascadeType.REFRESH})
+    @JoinColumn(name = "book_id")
     private Book book;
+
+    @Column(name = "quantity", nullable = false)
     private Integer quantity;
+
+    @Column(name = "price", nullable = false)
     private BigDecimal price;
+
+    @ManyToOne(cascade = {CascadeType.PERSIST,
+                    CascadeType.MERGE,
+                    CascadeType.REFRESH})
+    @JoinColumn(name = "order_id")
     private Long orderId;
 }
