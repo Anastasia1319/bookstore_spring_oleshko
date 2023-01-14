af7b7323a9c953d6b0bab087132c48e1020283cf
Anastasiya Oleshko <anastasiya.oleshko13@gmail.com>
2023-01-15 00:36:33 +0300

    Changed AppConfig to ApplicationConfig and added beans of EntityManagerFactory and TransactionManager

---
+++ src/main/java/com/belhard/bookstore/ApplicationConfig.java
@@ -0,0 +1,27 @@
+package com.belhard.bookstore;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.TransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+@Configuration
+@ComponentScan
+@EnableTransactionManagement
+public class ApplicationConfig {
+
+    @Bean
+    public EntityManagerFactory entityManagerFactory() {
+        return Persistence.createEntityManagerFactory("remote");
+    }
+
+    @Bean
+    public TransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory);
+    }
+}
