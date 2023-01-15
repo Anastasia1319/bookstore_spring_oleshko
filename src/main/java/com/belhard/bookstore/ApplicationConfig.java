7d5c5f5e3204a9cca7990f75d6f40d0fcb2984e2
Anastasiya Oleshko <anastasiya.oleshko13@gmail.com>
2023-01-15 21:38:01 +0300

    Added @EnableAspectJAutoProxi annotation in ApplicationConfig

--- src/main/java/com/belhard/bookstore/ApplicationConfig.java
+++ src/main/java/com/belhard/bookstore/ApplicationConfig.java
@@ -3,6 +3,7 @@ package com.belhard.bookstore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.transaction.TransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -13,6 +14,7 @@ import javax.persistence.Persistence;
 @Configuration
 @ComponentScan
 @EnableTransactionManagement
+@EnableAspectJAutoProxy
 public class ApplicationConfig {
 
     @Bean
