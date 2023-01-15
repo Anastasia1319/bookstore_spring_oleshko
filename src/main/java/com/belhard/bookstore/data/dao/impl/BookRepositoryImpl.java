4c4f841feb60059001cfaccab58ac89ceb621822
Anastasiya Oleshko <anastasiya.oleshko13@gmail.com>
2023-01-15 21:51:41 +0300

    Added annotation @Logging to all methods of BookRepositoryImpl

--- src/main/java/com/belhard/bookstore/data/dao/impl/BookRepositoryImpl.java
+++ src/main/java/com/belhard/bookstore/data/dao/impl/BookRepositoryImpl.java
@@ -1,8 +1,8 @@
 package com.belhard.bookstore.data.dao.impl;
 
+import com.belhard.bookstore.Logging;
 import com.belhard.bookstore.data.dao.BookRepository;
 import com.belhard.bookstore.data.entity.Book;
-import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -13,7 +13,6 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-@Log4j2
 @Transactional
 public class BookRepositoryImpl implements BookRepository {
     private static final String FIND_ALL = "from Book where deleted = false";
@@ -24,39 +23,36 @@ public class BookRepositoryImpl implements BookRepository {
     @PersistenceContext
     private EntityManager manager;
 
+    @Logging
     @Override
     public  List<Book> findAll() {
-        log.info("Created a list of books matching the search criteria");
         TypedQuery<Book> query = manager.createQuery(FIND_ALL, Book.class);
         return query.getResultList();
     }
 
+    @Logging
     @Override
     public Optional<Book> findById(Long id) {
-        log.info("Book with id {} found", id);
         TypedQuery<Book> query = manager.createQuery(FIND_BY_ID, Book.class);
         query.setParameter("id", id);
         return Optional.ofNullable(query.getSingleResult());
     }
 
+    @Logging
     @Override
     public void save (Book entity) {
-        log.info("Trying to update a row with a book in the database");
         if (entity.getId() != null) {
-            log.info("Update the book");
             manager.merge(entity);
         } else {
-            log.info("Create the book");
             manager.persist(entity);
         }
     }
 
+    @Logging
     @Override
     public boolean delete(Long id) {
-        log.info("Trying to delete a row with a book in the database");
         Book book = manager.createQuery(FIND_BY_ID, Book.class).setParameter("id", id).getSingleResult();
         if (book == null) {
-            log.warn("Updated rows on deletion (books): 0");
             return false;
         }
         book.setDeleted(true);
@@ -64,25 +60,25 @@ public class BookRepositoryImpl implements BookRepository {
         return true;
     }
 
+    @Logging
     @Override
     public Optional<Book> findByIsbn(String isbn) {
-        log.info("Book with isbn {} found", isbn);
         TypedQuery<Book> query = manager.createQuery(FIND_BY_ISBN, Book.class);
         query.setParameter("isbn", isbn);
         return Optional.ofNullable(query.getSingleResult());
     }
 
+    @Logging
     @Override
     public List<Book> findByAuthor(String author) {
-        log.info("Created a list of books matching the search criteria");
         TypedQuery<Book> query = manager.createQuery(FIND_BY_AUTHOR, Book.class);
         query.setParameter("author", author);
         return query.getResultList();
     }
 
+    @Logging
     @Override
     public long countAll() {
-        log.info("Created a database call - counting the number of rows");
         return findAll().size();
     }
 }
