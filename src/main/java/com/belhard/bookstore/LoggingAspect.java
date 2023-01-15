dbf4b2820cef587d2b75c1783d5f22d7121644ef
Anastasiya Oleshko <anastasiya.oleshko13@gmail.com>
2023-01-15 21:48:57 +0300

    Added methods for Logging

--- src/main/java/com/belhard/bookstore/LoggingAspect.java
+++ src/main/java/com/belhard/bookstore/LoggingAspect.java
@@ -1,6 +1,8 @@
 package com.belhard.bookstore;
 
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -9,8 +11,13 @@ import org.springframework.stereotype.Component;
 @Aspect
 public class LoggingAspect {
 
-    @Before()
-    public void adviceBefore() {
+    @Before("@annotation(Logging)")
+    public void adviceBefore(JoinPoint joinPoint) {
+        System.out.println("Arguments before calling a method" + joinPoint.getArgs());
+    }
 
+    @After("@annotation(Logging)")
+    public void adviceAfter (JoinPoint joinPoint) {
+        System.out.println("Arguments after calling a method" + joinPoint.getArgs());
     }
 }
