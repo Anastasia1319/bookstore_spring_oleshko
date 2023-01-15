365360269f3a529e214efbb6a30749e060b7fc53
Anastasiya Oleshko <anastasiya.oleshko13@gmail.com>
2023-01-15 21:31:44 +0300

    Created LoggingAspect

---
+++ src/main/java/com/belhard/bookstore/LoggingAspect.java
@@ -0,0 +1,16 @@
+package com.belhard.bookstore;
+
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class LoggingAspect {
+
+    @Before()
+    public void adviceBefore() {
+        
+    }
+}
