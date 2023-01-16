2ab4b9c0f8c449a8d6927c3d54cff825bd3b8a5b
Anastasiya Oleshko <anastasiya.oleshko13@gmail.com>
2023-01-16 12:43:54 +0300

    Changed methods in UserServiceImpl

--- src/main/java/com/belhard/bookstore/service/UserService.java
+++ src/main/java/com/belhard/bookstore/service/UserService.java
@@ -1,5 +1,6 @@
 package com.belhard.bookstore.service;
 
+import com.belhard.bookstore.data.entity.User;
 import com.belhard.bookstore.service.dto.UserServiceDto;
 
 import java.util.List;
@@ -8,8 +9,7 @@ public interface UserService {
     List<UserServiceDto> getAll();
     UserServiceDto getByEmail (String email);
     UserServiceDto getById (Long id);
-    UserServiceDto create (UserServiceDto dto);
-    UserServiceDto update (UserServiceDto dto);
+    void save (User user);
     void delete(Long id);
     UserServiceDto login(String email, String password);
     List<UserServiceDto> getAllWithNotActive();
--- src/main/java/com/belhard/bookstore/service/impl/UserServiceImpl.java
+++ src/main/java/com/belhard/bookstore/service/impl/UserServiceImpl.java
@@ -1,7 +1,8 @@
 package com.belhard.bookstore.service.impl;
 
+import com.belhard.bookstore.data.dao.UserRepository;
 import com.belhard.bookstore.data.entity.User;
-import com.belhard.bookstore.data.repository.UserRepository;
+
 import com.belhard.bookstore.exceptions.NotFoundException;
 import com.belhard.bookstore.exceptions.NotUpdateException;
 import com.belhard.bookstore.service.UserService;
@@ -23,7 +24,7 @@ public class UserServiceImpl implements UserService {
     @Override
     public List<UserServiceDto> getAll() {
         log.info("Received a list of users from UserDaoImpl");
-        return userRepository.findAll()
+        return userRepository.findAllActiveUsers()
                 .stream()
                 .sorted(Comparator.comparing(User::getId))
                 .map(converter::toUserDto)
@@ -32,47 +33,22 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserServiceDto getByEmail(String email) {
-        User user = userRepository.findByEmail(email);
-        log.info("The UserRepositoryImpl class method was called to search");
-        if (user == null) {
-            log.warn("User with email: {} not found!", email);
-            throw new NotFoundException("User with email: " + email + " not found!");
-        }
-        UserServiceDto userServiceDto = converter.toUserDto(user);
-        log.info("Search result: {}", userServiceDto);
-        return userServiceDto;
+        log.info("The UserRepository method was called to search by email");
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new NotFoundException("User with email " + email + " not found"));
+        return converter.toUserDto(user);
     }
 
     @Override
     public UserServiceDto getById(Long id) {
-        User user = userRepository.findById(id);
-        log.info("The UserRepositoryImpl class method was called to search");
-        if (user == null) {
-            log.warn("User with email: {} not found!", id);
-            throw new NotFoundException("User with id: " + id + " not found!");
-        }
-        UserServiceDto userServiceDto = converter.toUserDto(user);
-        log.info("Search result: {}", userServiceDto);
-        return userServiceDto;
+        log.info("The UserRepository method was called to search by id");
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("User with id " + id + " not found"));
+        return converter.toUserDto(user);
     }
 
-    @Override
-    public UserServiceDto create(UserServiceDto dto) {
-        validate(dto);
-        UserServiceDto existing = converter.toUserDto(userRepository.findByEmail(dto.getEmail()));
-        if (existing != null) {
-            log.error("A user with this email already exists in the database");
-            throw new NotUpdateException("Can't create: a user with this email already exists in the database");
-        }
-        User toCreate = converter.toUserEntity(dto);
-        User created = userRepository.create(toCreate);
-        UserServiceDto userServiceDto = converter.toUserDto(created);
-        log.info("Creation result: {}", userServiceDto);
-        return userServiceDto;
-    }
-
-    private void validate (UserServiceDto dto) {
-        if (dto.getPassword().length() < 8) {
+    private void validate (User user) {
+        if (user.getPassword().length() < 8) {
             log.error("Password shorter 8 characters");
             throw new NotUpdateException("Password cannot be shorter than 8 characters.");
         }
@@ -80,44 +56,31 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public UserServiceDto update(UserServiceDto dto) {
-        validate(dto);
-        UserServiceDto existing = converter.toUserDto(userRepository.findByEmail(dto.getEmail()));
-        if (existing != null && existing.getId() != dto.getId()) {
-            log.error("A user with this email already exists in the database");
-            throw new NotUpdateException("Can't update: a user with this email already exists in the database");
-        }
-        User toUpdate = converter.toUserEntity(dto);
-        User updated = userRepository.update(toUpdate);
-        UserServiceDto userServiceDto = converter.toUserDto(updated);
-        log.info("Update result: {}", userServiceDto);
-        return userServiceDto;
+    public void save(User user) {
+        validate(user);
+        userRepository.save(user);
     }
 
     @Override
     public void delete(Long id) {
-        if (!userRepository.delete(id)) {
-            log.error("User with id {} not deleted", id);
-            throw new NotFoundException("Couldn't delete user with id: " + id + "!");
-        }
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("User with id " + id + " not found"));
+        user.setActive(false);
+        userRepository.save(user);
         log.info("User with id {} deleted", id);
     }
 
     public UserServiceDto login(String email, String password) {
-        User user = userRepository.findByEmail(email);
-        if (user == null || !password.equals(user.getPassword())) {
-            log.error("Incorrect email or password");
-            throw new NotFoundException("User with email: " + email + "and with password: " + password + " not found!");
-        }
-        UserServiceDto userServiceDto = converter.toUserDto(user);
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new NotFoundException("User with email " + email + " not found"));
         log.info("Login completed");
-        return userServiceDto;
+        return converter.toUserDto(user);
     }
 
     @Override
     public List<UserServiceDto> getAllWithNotActive() {
         log.info("Received a list of all users (active and not-active) from UserDaoImpl");
-        return userRepository.findAllWithNotActive()
+        return userRepository.findAll()
                 .stream()
                 .sorted(Comparator.comparing(User::getId))
                 .map(converter::toUserDto)
