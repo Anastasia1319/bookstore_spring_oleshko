89cbdf7bd1a5e4e8027e183215c4dc2b160ef822
Anastasiya Oleshko <anastasiya.oleshko13@gmail.com>
2023-01-17 21:08:36 +0300

    Added annotation Param in methods of BookRepository

--- src/main/java/com/belhard/bookstore/data/dao/BookRepository.java
+++ src/main/java/com/belhard/bookstore/data/dao/BookRepository.java
@@ -3,6 +3,7 @@ package com.belhard.bookstore.data.dao;
 import com.belhard.bookstore.data.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,14 +11,14 @@ import java.util.Optional;
 public interface BookRepository extends JpaRepository<Book, Long> {
 
     @Query("from Book b where b.deleted = false and b.isbn = :isbn")
-    Optional<Book> findByIsbn (String isbn);
+    Optional<Book> findByIsbn (@Param("isbn") String isbn);
 
     @Query("from Book b where b.deleted = false and b.author = :author")
-    List<Book> findByAuthor (String author);
+    List<Book> findByAuthor (@Param("author") String author);
 
     @Query("from Book b where b.deleted = false")
     List<Book> findAllAvailable ();
 
     @Query("from Book b where b.deleted = false and b.id = :id")
-    Optional<Book> findAvailableById (Long id);
+    Optional<Book> findAvailableById (@Param("id") Long id);
 }
