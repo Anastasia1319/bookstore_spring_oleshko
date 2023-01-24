0747e9d16f8893f4faa9482c8fc14b891998ab3d
Anastasiya Oleshko <anastasiya.oleshko13@gmail.com>
2023-01-24 18:02:20 +0300

    Chenged javax.servlet version. Added onStartup method in WebInitializer

--- src/main/java/com/belhard/bookstore/WebInitializer.java
+++ src/main/java/com/belhard/bookstore/WebInitializer.java
@@ -1,14 +1,23 @@
 package com.belhard.bookstore;
 
 import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
 
 public class WebInitializer implements WebApplicationInitializer {
 
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
-        
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.register(ApplicationConfig.class);
+
+        DispatcherServlet servlet = new DispatcherServlet(context);
+        ServletRegistration.Dynamic registration = servletContext.addServlet("DispatcherServlet", servlet);
+        registration.setLoadOnStartup(1);
+        registration.addMapping("/");
     }
 }
