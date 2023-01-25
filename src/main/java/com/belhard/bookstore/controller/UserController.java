e07db0c7bf7e32dd1b43b207d78dd06d41fac9a5
Anastasiya Oleshko <anastasiya.oleshko13@gmail.com>
2023-01-25 23:02:51 +0300

    Created getAll method in UserController

--- src/main/java/com/belhard/bookstore/controller/UserController.java
+++ src/main/java/com/belhard/bookstore/controller/UserController.java
@@ -4,11 +4,16 @@ package com.belhard.bookstore.controller;
 import com.belhard.bookstore.service.UserService;
 import com.belhard.bookstore.service.dto.UserDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/users")
@@ -21,5 +26,17 @@ public class UserController {
         UserDto user = userService.getById(id);
         model.addAttribute("user", user);
         return "user";
+    }
+
+    @GetMapping("/{page}")
+    public String getAll(@PathVariable Integer page, Model model) {
+        Sort sort = Sort.by(Sort.Direction.ASC, "id");
+        int pageSize = 5;
+        Pageable pageable = PageRequest.of(page, pageSize, sort);
+        Long totalPages = userService.totalPagesActive(pageSize);
+        List<UserDto> users = userService.getAll(pageable);
+        model.addAttribute("users", users);
+        model.addAttribute("totalPages", totalPages);
+        return "users";
     }
 }
