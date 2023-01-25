382b2f421e42a777ea112e29c8dccd3d96b9502d
Anastasiya Oleshko <anastasiya.oleshko13@gmail.com>
2023-01-25 16:08:44 +0300

    Created getUser method in UserController

--- src/main/java/com/belhard/bookstore/controller/UserController.java
+++ src/main/java/com/belhard/bookstore/controller/UserController.java
@@ -1,10 +1,25 @@
 package com.belhard.bookstore.controller;
 
 
+import com.belhard.bookstore.service.UserService;
+import com.belhard.bookstore.service.dto.UserDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/users")
+@RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/{id}")
+    public String getUser(@PathVariable Long id, Model model) {
+        UserDto user = userService.getById(id);
+        model.addAttribute("user", user);
+        return "user";
+    }
 }
