dada1767563b50f7d9bd8ed9d7d08371a4849bf7
Anastasiya Oleshko <anastasiya.oleshko13@gmail.com>
2023-01-26 16:56:34 +0300

    Created delete method in UserController

--- src/main/java/com/belhard/bookstore/controller/UserController.java
+++ src/main/java/com/belhard/bookstore/controller/UserController.java
@@ -61,4 +61,10 @@ public class UserController {
         userService.save(user);
         return "redirect:/users/" + user.getId();
     }
+
+    @PostMapping("/delete/{id}")
+    public String delete(@PathVariable Long id) {
+        userService.delete(id);
+        return "redirect:/users/page=0";
+    }
 }
