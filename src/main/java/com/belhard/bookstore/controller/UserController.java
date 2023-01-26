a7c6fe35859b58d7dbf648c8aa3bf535ae53e78d
Anastasiya Oleshko <anastasiya.oleshko13@gmail.com>
2023-01-26 16:47:17 +0300

    Created editUserForm and editUser methods in UserController

--- src/main/java/com/belhard/bookstore/controller/UserController.java
+++ src/main/java/com/belhard/bookstore/controller/UserController.java
@@ -49,5 +49,16 @@ public class UserController {
         return "redirect:/users/" + user.getId();
     }
 
+    @GetMapping("/edit/{id}")
+    public String editUserForm(@PathVariable Long id, Model model) {
+        UserDto user = userService.getById(id);
+        model.addAttribute("user", user);
+        return "edit_user";
+    }
 
+    @PostMapping("/edit/{id}")
+    public String editUser(@ModelAttribute UserDto user) {
+        userService.save(user);
+        return "redirect:/users/" + user.getId();
+    }
 }
