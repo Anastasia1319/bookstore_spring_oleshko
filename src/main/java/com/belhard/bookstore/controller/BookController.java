3419604db0be4079e67c0492e2fafb09e90c0213
Anastasiya Oleshko <anastasiya.oleshko13@gmail.com>
2023-01-26 23:32:16 +0300

    Added gettAll method in BookController

--- src/main/java/com/belhard/bookstore/controller/BookController.java
+++ src/main/java/com/belhard/bookstore/controller/BookController.java
@@ -3,11 +3,16 @@ package com.belhard.bookstore.controller;
 import com.belhard.bookstore.service.BookService;
 import com.belhard.bookstore.service.dto.BookDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/books")
@@ -20,5 +25,17 @@ public class BookController {
         BookDto book = bookService.getById(id);
         model.addAttribute("book", book);
         return "book";
+    }
+
+    @GetMapping("/page={page}")
+    public String getAll(@PathVariable Integer page, Model model) {
+        Sort sort = Sort.by(Sort.Direction.ASC, "id");
+        int pageSize = 5;
+        Pageable pageable = PageRequest.of(page, pageSize, sort);
+        Long totalPages = bookService.totalPages(pageSize);
+        List<BookDto> books = bookService.getAll(pageable);
+        model.addAttribute("books", books);
+        model.addAttribute("totalPages");
+        return "books";
     }
 }
