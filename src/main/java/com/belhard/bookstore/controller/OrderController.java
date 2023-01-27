5e5cf468bae0da2c6ebffcf949da2b2a175de8a4
Anastasiya Oleshko <anastasiya.oleshko13@gmail.com>
2023-01-27 22:28:48 +0300

    Added getAll method in OrderController

--- src/main/java/com/belhard/bookstore/controller/OrderController.java
+++ src/main/java/com/belhard/bookstore/controller/OrderController.java
@@ -3,11 +3,16 @@ package com.belhard.bookstore.controller;
 import com.belhard.bookstore.service.OrderService;
 import com.belhard.bookstore.service.dto.OrderDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/orders")
@@ -20,5 +25,17 @@ public class OrderController {
         OrderDto order = orderService.getById(id);
         model.addAttribute("order", order);
         return "order";
+    }
+
+    @GetMapping("/page={page}")
+    public String getAll(@PathVariable Integer page, Model model) {
+        Sort sort = Sort.by(Sort.Direction.ASC, "id");
+        int pageSize = 5;
+        Pageable pageable = PageRequest.of(page, pageSize, sort);
+        Long totalPages = orderService.totalPages(pageSize);
+        List<OrderDto> orders = orderService.getAll(pageable);
+        model.addAttribute("orders", orders);
+        model.addAttribute("totalPages", totalPages);
+        return "orders";
     }
 }
