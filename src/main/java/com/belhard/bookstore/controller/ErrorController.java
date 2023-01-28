73eb3e0f83b9ad219704e7c3bdf689fb660dda44
Anastasiya Oleshko <anastasiya.oleshko13@gmail.com>
2023-01-28 16:45:52 +0300

    Created ErrorController

---
+++ src/main/java/com/belhard/bookstore/controller/ErrorController.java
@@ -0,0 +1,41 @@
+package com.belhard.bookstore.controller;
+
+import com.belhard.bookstore.exceptions.ApplicationException;
+import com.belhard.bookstore.exceptions.NotFoundException;
+import com.belhard.bookstore.exceptions.NotUpdateException;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@ControllerAdvice
+@RequestMapping("/error")
+public class ErrorController {
+
+    @GetMapping
+    public String error() {
+        return "error";
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String applicationError(NotFoundException e, Model model) {
+        model.addAttribute("message", e.getMessage());
+        return "error";
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String applicationError(NotUpdateException e, Model model) {
+        model.addAttribute("message", e.getMessage());
+        return "error";
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String applicationError(ApplicationException e, Model model) {
+        model.addAttribute("message", e.getMessage());
+        return "error";
+    }
+}
