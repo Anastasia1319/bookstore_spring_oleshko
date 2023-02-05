75bbd091617d997acc6e2816b3ccafe98eebf188
Anastasiya Oleshko <anastasiya.oleshko13@gmail.com>
2023-02-05 17:33:36 +0300

    Created SecurityException

---
+++ src/main/java/com/belhard/bookstore/exceptions/SecurityException.java
@@ -0,0 +1,7 @@
+package com.belhard.bookstore.exceptions;
+
+public class SecurityException extends RuntimeException{
+    public SecurityException (String message) {
+        super(message);
+    }
+}
