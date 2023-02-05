bd9b4795cd4c667a4e6ecd1b4cf79487d523a251
Anastasiya Oleshko <anastasiya.oleshko13@gmail.com>
2023-02-05 23:37:02 +0300

    Created Authorization filter

---
+++ src/main/java/com/belhard/bookstore/web/filter/AuthorizationFilter.java
@@ -0,0 +1,23 @@
+package com.belhard.bookstore.web.filter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = {"/books/create", "/books/delete", "books/edit", "books/", "/orders/*", "users/*"})
+public class AuthorizationFilter extends HttpFilter {
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain)
+            throws IOException, ServletException {
+        Object user = req.getSession().getAttribute("user");
+        if (user == null) {
+            res.sendRedirect("/login");
+            return;
+        }
+        chain.doFilter(req, res);
+    }
+}
