c66910eb2c2ef0688297fd5d3925b53d246116c1
Anastasiya Oleshko <anastasiya.oleshko13@gmail.com>
2023-02-06 20:28:12 +0300

    Created AuthorizationFilter

---
+++ src/main/java/com/belhard/bookstore/web/filter/AuthenticationFilter.java
@@ -0,0 +1,29 @@
+package com.belhard.bookstore.web.filter;
+
+import com.belhard.bookstore.data.entity.Role;
+import com.belhard.bookstore.service.dto.UserDto;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter(filterName = "AuthenticationFilter")
+public class AuthenticationFilter extends HttpFilter {
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain)
+            throws IOException, ServletException {
+        Object object = req.getSession().getAttribute("user");
+        UserDto user = (UserDto) object;
+        if (user.getRole() == Role.CUSTOMER) {
+//            res.setStatus(403);
+//            res.sendError(HttpServletResponse.SC_FORBIDDEN, "Not Enough Rights!");
+            res.sendRedirect("/error");
+            return;
+        }
+        chain.doFilter(req, res);
+    }
+}
